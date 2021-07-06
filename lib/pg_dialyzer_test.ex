22dbcdf4cfe596b0131339b79b8d1d3b305cef54
Matt Jones <al2o3cr@gmail.com>
2021-07-05 20:29:27 -0400

    reproduces error

---
+++ lib/pg_dialyzer_test.ex
@@ -0,0 +1,18 @@
+defmodule PgDialyzerTest do
+  @moduledoc """
+  Documentation for `PgDialyzerTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PgDialyzerTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pg_dialyzer_test_test.exs
@@ -0,0 +1,8 @@
+defmodule PgDialyzerTestTest do
+  use ExUnit.Case
+  doctest PgDialyzerTest
+
+  test "greets the world" do
+    assert PgDialyzerTest.hello() == :world
+  end
+end
